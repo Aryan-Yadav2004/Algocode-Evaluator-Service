cb63153b929834d0c2edc300ab81ecea1ba305fa
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-12-24 18:57:41 +0530

    Added ESlint configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default tseslint.config(
+{
+  languageOptions:{
+    parserOptions:{
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+    }
+  },
+  files: ["src/**/*.ts"],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+  },
+  rules: {
+    quotes: ['error','single',{allowTemplateLiterals: true}],
+    semi: ["error", "always"],
+    "no-multi-spaces": "error",
+    "no-multiple-empty-lines": "error",
+    "no-unused-vars": ["warn"],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  }
+}
+);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express from "express";
-import serverConfig from "./config/server.config.js";
+import express from 'express';
+
+import serverConfig from './config/server.config.js';
+
 const app = express();
 
 app.listen(serverConfig.PORT, () => {
-    console.log("server started at *:" + 3000);
+    console.log('server started at *:' + 3000);
 });
-
+ 
+ 
